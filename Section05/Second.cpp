57a2c210e33331b07c6148b608937788d39cccd0
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-24 02:07:53 +0530

    String Introduction

---
+++ Section05/Second.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    
+    string Name ;
+    cout<< " May i know your name :";
+    // cin>> Name;
+    getline(cin,Name);
+    
+    cout<< " Welcome mr./mrs."<< Name ;
+    cout<< endl;
+     
+    
+    return 0;
+     
+}

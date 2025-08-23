02eed8177170eb56de8ef7a0506209e9d738ae9d
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-24 01:53:01 +0530

    Adding Two Number

---
+++ Section05/firstpro.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+ 
+int main ()
+{
+    int a,b,c;
+    cout<< " Enter the Value of a  :";
+    cin>> a  ;
+    
+    cout<< " Enter the Value of b  :";
+    cin>> b  ;
+    c=(a+b);
+    cout<< " Sum of number is :"<< c ;
+    
+    
+     
+    
+    return 0;
+     
+}

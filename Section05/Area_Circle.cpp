6923fcce6d82d5a2e4e7a811b9257e30b27c0320
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-25 15:01:17 +0530

    Area of circle

---
+++ Section05/Area_Circle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+ 
+
+int main ()
+{
+    cout<< " Enter the Radius of circle :";
+    double r;
+    cin>> r;
+    
+    double Area=3.14*r*r;
+    cout<< " Final Area of circle is :"<< Area<< endl;
+     
+     return 0;
+      
+}

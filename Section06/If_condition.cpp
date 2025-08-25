b04095793d0db6812fe67db6486176767a296a04
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-25 22:31:00 +0530

    Maximum of Two number

---
+++ Section06/If_condition.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int x,y;
+    cout<< " Enter the value of x :";
+    cin>> x;
+    cout<< " Enter the value of y :";
+    cin>> y;
+    if(x>y)
+    {
+        cout<< " X is greater than y"<< x<< endl;
+        
+    }
+    else 
+    cout<< " Y is Greater than x "<<  y<< endl;
+    return 0;
+     
+}

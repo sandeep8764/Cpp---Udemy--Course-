0489ff564f2e565ed59e71817663b377d3ff5553
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-25 12:25:25 +0530

    Area of Triangle

---
+++ Section05/Area_Triangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+
+int main ()
+{
+    double length , breadth ;
+    double Area;
+    
+    cout<< " Enter the length :";
+    cin>> length ;
+    cout<< " Enter the breadth :";
+    cin>> breadth;
+    Area=(length*breadth)/2;
+    cout<< " Final Area of triangle is :"<< Area<< endl;
+     
+    
+    
+    return 0;
+     
+}

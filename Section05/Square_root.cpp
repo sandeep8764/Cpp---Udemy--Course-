5e384536ac293cae1a7d5480faf82fc442d429a7
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-25 12:42:25 +0530

    Square root of number

---
+++ Section05/Square_root.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std ;
+int main ()
+{
+    int n ;
+    cout<< " Enter the value of n :";
+    cin>> n ;
+     double Root=sqrt(n);
+     cout<< " Final Root of number is :"<< Root<< endl;
+     
+     
+    return 0;
+     
+}

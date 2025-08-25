8ee03cfdaae2887358c1bd76adc9f22189ecc7d9
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-25 17:17:46 +0530

    Bitwise operator

---
+++ Section05/Operator/Bitwisw_operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int x=11,y=5;
+    int z=x&y;
+    cout<< " and Operator :"<< z<< endl;
+    int Z2=x|y;
+    cout<< " Or operator :"<< Z2<<endl;
+    int Z3=x^y;
+    cout<< " Or operator :"<< Z3<<endl;
+    
+    
+    
+    return 0;
+    
+}

7d6c975370065dd64524fffdb350ca7184c46000
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-25 12:33:35 +0530

    Sum of n natural number

---
+++ Section05/Sum_N_Num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+int main ()
+
+{
+    int n;
+    cout<< " Enter the value of n :";
+    cin>> n ;
+    double Sum =n*(n+1)/2;
+    cout<< " Final Sum of n natural number is :"<< Sum<< endl;
+    
+    
+    return 0;
+    
+}

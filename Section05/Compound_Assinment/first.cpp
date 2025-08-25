37d99933b9e2a4e6f63de72e9e5dd368628354b1
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-25 15:29:18 +0530

    Compound Assinment

---
+++ Section05/Compound_Assinment/first.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    
+    int a=10,b=5;
+    int sum =10;
+    sum+=a;// sum =sum +a
+    int product =2;
+    product*=b; // product =product*b
+    cout<< sum << endl;
+    cout<< product<< endl;
+     
+    
+     return 0;
+     
+}

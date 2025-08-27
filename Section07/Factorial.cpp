0bf01bf8fd722909d6666b17e34bc1accffb0faf
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-27 15:46:44 +0530

    Factorial of number

---
+++ Section07/Factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+    int n,fact=1;
+    cout<< " Enter the value of n :";
+    cin>> n;
+    for (int i=1;i<=n;i++)
+    {
+        fact*=i;
+        
+    }
+    cout<< " Factorail of number is :"<< fact<< endl;
+    
+    
+    return 0;
+     
+}

9aeb62c5d51836288022f9e2690a763a102dde33
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-29 13:02:06 +0530

    Introduction to pointer

---
+++ Section09/Pointer_intro.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+    // Introduction to pointer 
+    int a=10;
+    int *p=&a;  // Interger type pointer that will contain address of a 
+    cout<< a<< endl;
+    cout<< &a<< endl;
+    cout<< p<< endl;
+    cout<< &p<<endl;
+    cout<< *p<< endl; // Derrefrencing 
+    
+    
+    
+    
+    
+    
+    return 0;
+    
+}

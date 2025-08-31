c2b955f2fe5d2900f3de0dd232b106a934ca7360
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-31 23:33:29 +0530

    Add Using Function

---
+++ Section11/Func_Add.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std ;
+
+// Function Creating 
+
+int  Add (int x, int y)
+{
+    int z=(x+y);
+    return z;
+    
+    
+}
+int  main()
+ {
+    int a=10,b=20;
+    
+    
+    // Function calling 
+    //Add(a,b);
+    cout<< " Sum of number is :"<< Add(a,b)<<endl;
+    
+   
+    
+    return 0;
+    
+ }

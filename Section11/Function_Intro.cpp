aba3e4f97c8786b559266e55ed8f378f41286119
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-08-31 23:20:34 +0530

    Function introduction

---
+++ Section11/Function_Intro.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std ;
+
+// Function Creating 
+
+void Display ()
+{
+    cout<< " Hello to function "<<endl;
+    
+}
+int  main()
+ {
+    
+    // Function calling 
+    Display();
+    
+    return 0;
+    
+ }

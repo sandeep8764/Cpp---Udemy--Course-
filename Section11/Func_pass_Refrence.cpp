f640c23a09260b4e8d1000490afbac631a0bd3a4
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-09-01 15:38:04 +0530

    Pass by Refrence

---
+++ Section11/Func_pass_Refrence.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std ;
+
+// Function Creating 
+
+void Swap (int &a,int &b)
+{
+    cout<< a<< " "<<b<< endl;
+    int temp=a;
+    a=b;
+    b=temp;
+     cout<< a<< " "<<b<< endl;
+    
+    
+}
+int  main()
+ {
+    int x=10,y=20;
+    // Function calling 
+   Swap(x,y);
+   cout<< x<< "  "<< y<< endl;
+   
+    
+    return 0;
+    
+ }

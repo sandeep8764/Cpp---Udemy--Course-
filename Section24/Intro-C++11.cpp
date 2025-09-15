bc6b1dfb5ce03af7768ffb9a6915801d8dbb03c3
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-09-15 15:56:04 +0530

    C++11 version

---
+++ Section24/Intro-C++11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+// float fun
+// {
+//     return 2.34f;
+    
+// }
+
+
+int main()
+{
+    auto x=2*5.56+'a';
+    cout<< x<< endl;
+    
+    return 0;
+}
